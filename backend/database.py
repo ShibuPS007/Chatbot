bb89fdfecc9730d06eb50e32d31bd791c8e60ca5
ShibuPS007 <psshibu2004@gmail.com>
2026-02-13 14:48:19 +0530

    fix database path for GitHub Actions

--- backend/database.py
+++ backend/database.py
@@ -1,18 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import  os
+import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 IN_DOCKER = os.path.exists("/.dockerenv")
 
 if IN_DOCKER:
     DATABASE_URL = "sqlite:////app/backend/chat.db"
 else:
-    DATABASE_URL = "sqlite:///backend/chat.db" 
+    DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'chat.db')}"
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
+
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
