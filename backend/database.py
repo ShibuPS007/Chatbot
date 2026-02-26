da160188b4a91b0f169ccffe713a8a7dd11815a0
ShibuPS007 <psshibu2004@gmail.com>
2026-02-26 11:40:20 +0530

    made changes in database.py adding sqlite

--- backend/database.py
+++ backend/database.py
@@ -4,6 +4,14 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+)
+
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
